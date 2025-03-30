28242f461ddb244e98acb11d534dff05b0ffbf06
Sergio Estrada <esergio@wustl.edu>
2025-03-29 23:29:02 -0500

    update

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-REPO_DIR="/export/home/containerweb/ENGR-SVC-Makertech/deployable/makerspace"
-GITHUB_USER="esergio999"
-GITHUB_TOKEN="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"
+REPO_DIR=""
+GITHUB_USER=""
+GITHUB_TOKEN=""
 REPO_URL="https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/McKelvey-Engineering-IT/makerspace.git"
 DEF_FILE="app.def"
 SIF_FILE="app.sif"
